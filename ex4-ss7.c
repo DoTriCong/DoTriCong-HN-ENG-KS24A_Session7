2251fedd1aee1a3a71a06c517d61019923498cdd
DoTriCong <tricong5526@gmail.com>
2024-11-28 09:47:30 +0700

    Add files via upload

---
+++ ex4-ss7.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main(){
+	int number;
+	printf("Moi ban nhap so phan tu cua mang: \n");
+	scanf("%d",&number);
+	
+	int a[number];
+	for(int i=1; i<=number; i++){
+		printf("Moi ban nhap phan tu thu %d cua mang: \n",i);
+		scanf("%d",&a[i]);
+	}
+	for(int i=1;i<=number;i++){
+		printf("array[%d] la: %d\n",i,a[i]);
+	}
+	return 0; 
+}
